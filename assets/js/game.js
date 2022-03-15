a5c13d2dfe2a6058b7e9ab55f483f5ea3514babb
Michael <michaelstevendepalma@gmail.com>
2022-03-15 10:29:22 -0400

    added robots name

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,14 @@
 function fight() {
     window.alert("The fight has begun!");
   }
+
   fight();
+  
  var playerName = window.prompt("What is your robot's name?");
- 
+ console.log(playerName);
+
+ var name = "your name";
+ console.log(name);
+
+
+ // fight();
